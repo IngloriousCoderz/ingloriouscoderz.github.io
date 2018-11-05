eaa6cbdb9a6d9702cc4099928ae52af7192e385a
IceOnFire <antony.mistretta@gmail.com>
2018-11-05 12:18:56 +0100

    Fix external link on head being interpreted as internal

--- components/button.js
+++ components/button.js
@@ -1,24 +1,33 @@
 import Link from 'next/link'
 import { withI18n } from 'react-i18next'
 
-export default withI18n()(({ id, href, title, target, rel, t }) => (
-  <Link href={href || `/${id}`} prefetch>
-    <a target={target} rel={rel}>
-      {t(title)}
-      <style jsx>{`
-        a {
-          flex: 1;
-          background-color: rgba(40, 44, 52, 0.5);
-          margin: 0.25rem;
-          border: 1px solid #429aef;
-          padding: 0.25rem;
-          text-transform: uppercase;
-        }
+const Button = ({ id, href, target, rel, title, t }) => (
+  <a href={href || `/${id}`} target={target} rel={rel}>
+    {t(title)}
+    <style jsx>{`
+      a {
+        flex: 1;
+        background-color: rgba(40, 44, 52, 0.5);
+        margin: 0.25rem;
+        border: 1px solid #429aef;
+        padding: 0.25rem;
+        text-transform: uppercase;
+      }
 
-        a:hover {
-          text-decoration: none;
-        }
-      `}</style>
-    </a>
-  </Link>
-))
+      a:hover {
+        text-decoration: none;
+      }
+    `}</style>
+  </a>
+)
+
+export default withI18n()(props => {
+  const { id, href, target } = props
+  return target ? (
+    Button(props)
+  ) : (
+    <Link prefetch href={href || `/${id}`}>
+      {Button(props)}
+    </Link>
+  )
+})
