9ddf718eab8773525c5d9ad2083d89566754a044
IceOnFire <antony.mistretta@gmail.com>
2018-12-11 19:50:58 +0100

    Re-enable service workers

--- next.config.js
+++ next.config.js
@@ -48,16 +48,46 @@ module.exports = compose(
 
       config.plugins.push(
         new SWPrecacheWebpackPlugin({
-          // NOTE: this line is not present here: https://github.com/zeit/next.js/blob/canary/examples/with-sw-precache/next.config.js
-          // navigateFallback: '/index',
-          verbose: true,
-          staticFileGlobsIgnorePatterns: [/\.next\//],
-          runtimeCaching: [
-            {
-              handler: 'networkFirst',
-              urlPattern: /^https?.*/,
-            },
+          // NOTE: configuration copied and merged from create-react-app and with-sw-precache
+
+          // By default, a cache-busting query parameter is appended to requests
+          // used to populate the caches, to ensure the responses are fresh.
+          // If a URL is already hashed by Webpack, then there is no concern
+          // about it being stale, and the cache-busting can be skipped.
+          dontCacheBustUrlsMatching: /\.\w{8}\./,
+          filename: 'service-worker.js',
+          logger(message) {
+            if (message.indexOf('Total precache size is') === 0) {
+              // This message occurs for every build and is a bit too noisy.
+              return
+            }
+            if (message.indexOf('Skipping static resource') === 0) {
+              // This message obscures real errors so we ignore it.
+              // https://github.com/facebookincubator/create-react-app/issues/2612
+              return
+            }
+            console.log(message)
+          },
+          minify: true,
+          // For unknown URLs, fallback to the index page
+          navigateFallback: '/index.html',
+          // Ignores URLs starting from /__ (useful for Firebase):
+          // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
+          navigateFallbackWhitelist: [/^(?!\/__).*/],
+          // Don't precache sourcemaps (they're large) and build asset manifest:
+          staticFileGlobsIgnorePatterns: [
+            /\.map$/,
+            /asset-manifest\.json$/,
+            /\.next\//,
           ],
+
+          // verbose: true,
+          // runtimeCaching: [
+          //   {
+          //     handler: 'networkFirst',
+          //     urlPattern: /^https?.*/,
+          //   },
+          // ],
         }),
       )
 
--- pages/_app.js
+++ pages/_app.js
@@ -27,14 +27,14 @@ export default enhance(
     }
 
     componentDidMount() {
-      // if ('serviceWorker' in navigator) {
-      //   navigator.serviceWorker
-      //     .register('/service-worker.js')
-      //     .then(() => console.log('service worker registered.'))
-      //     .catch(err =>
-      //       console.warn('service worker registration failed.', err.message),
-      //     )
-      // }
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+          .register('/service-worker.js')
+          .then(() => console.log('service worker registered.'))
+          .catch(err =>
+            console.warn('service worker registration failed.', err.message),
+          )
+      }
     }
 
     render() {
