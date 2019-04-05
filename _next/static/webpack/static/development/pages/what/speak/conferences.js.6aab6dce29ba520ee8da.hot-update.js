90447e1cd2f0996a057063fc961400981f2796bc
IceOnFire <antony.mistretta@gmail.com>
2019-04-05 09:14:26 +0200

    Add conference held in Amsterdam

---
+++ _next/static/webpack/static/development/pages/what/speak/conferences.js.6aab6dce29ba520ee8da.hot-update.js
@@ -0,0 +1,287 @@
+webpackHotUpdate("static/development/pages/what/speak/conferences.js",{
+
+/***/ "./pages/what/speak/conferences.js":
+/*!*****************************************!*\
+  !*** ./pages/what/speak/conferences.js ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_i18next__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-i18next */ "./node_modules/react-i18next/dist/es/index.js");
+/* harmony import */ var _layouts_default__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../layouts/default */ "./layouts/default/index.js");
+/* harmony import */ var _components_row__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../components/row */ "./components/row.js");
+/* harmony import */ var _components_column__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../../components/column */ "./components/column.js");
+/* harmony import */ var _components_card__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../../components/card */ "./components/card.js");
+/* harmony import */ var _components_ext_link__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../../../components/ext-link */ "./components/ext-link.js");
+var _jsxFileName = "/home/antony/Progetti/Inglorious Coderz/ingloriouscoderz.github.io/pages/what/speak/conferences.js";
+
+
+
+
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_layouts_default__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    path: "what/speak/conferences",
+    title: "Conferences",
+    description: "Talks and keynotes performed worldwide.",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_i18next__WEBPACK_IMPORTED_MODULE_1__["Trans"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("article", {
+    className: "card card-1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, "Conferences"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, "One way to change the world is by inspiring people and hitting them with exciting pills of knowledge. Inglorious Coderz know how to choose a suitable subject and then convey the message in the most engaging, fun, and effective way.")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_row__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_column__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    max: 3,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_card__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    title: "React, The Inglorious Way",
+    image: __webpack_require__(/*! ../../../static/images/what/speak/react_inglorious_way.jpg */ "./static/images/what/speak/react_inglorious_way.jpg"),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }, "\"React, The Inglorious Way\" is a 40 minute talk performed at", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://events.codemotion.com/conferences/amsterdam/2019/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, "Codemotion Amsterdam 2019"), ". It is a highly technical talk performed as a live coding session."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39
+    },
+    __self: this
+  }, "The slides, made with", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://mdx-deck.jxnblk.com/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 41
+    },
+    __self: this
+  }, "mdx-deck"), ", are available ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "/react-the-inglorious-way",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42
+    },
+    __self: this
+  }, "here"), ", while a PDF version can be downloaded", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "/react-the-inglorious-way/presentation.pdf",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 44
+    },
+    __self: this
+  }, "here"), "."))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_column__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    max: 3,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 52
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_card__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    title: "Refactoring Into React Hooks",
+    image: __webpack_require__(/*! ../../../static/images/what/speak/hooks.jpg */ "./static/images/what/speak/hooks.jpg"),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 53
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 56
+    },
+    __self: this
+  }, "\"Refactoring Into React Hooks\" is a 40 minute talk performed at", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://events.codemotion.com/conferences/rome/2019/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58
+    },
+    __self: this
+  }, "Codemotion Rome 2019"), ". It is a highly technical talk performed as a live coding session that shows how to turn your codebase into hooks starting from class components, higher-order components, render props, context API and much more."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 66
+    },
+    __self: this
+  }, "The slides, made with", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://mdx-deck.jxnblk.com/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 68
+    },
+    __self: this
+  }, "mdx-deck"), ", are available", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "/refactoring-into-react-hooks",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 70
+    },
+    __self: this
+  }, "here"), ", while a PDF version can be downloaded", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "/refactoring-into-react-hooks/presentation.pdf",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 72
+    },
+    __self: this
+  }, "here"), "."))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_column__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    max: 3,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 80
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_card__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    title: "Technology 101 For Startupperz",
+    image: __webpack_require__(/*! ../../../static/images/what/speak/apia.jpg */ "./static/images/what/speak/apia.jpg"),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 81
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 84
+    },
+    __self: this
+  }, "\"Tech 101\" is a 40 minute keynote requested by", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://www.inacademy.eu/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 86
+    },
+    __self: this
+  }, "European Innovation Academy"), ' ', "and performed in Turin, Shenzhen, and Doha. The slides, available on", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://www.slideshare.net/InnovationAcademy/apia2018-antony-mistretta-tech-101?qid=80340f9c-4dd9-4933-85ef-938fa0a0a84b",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 91
+    },
+    __self: this
+  }, "Slideshare"), ", are funny hand-drawn doodles scanned and composed on Google Slides."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 97
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://youtu.be/Ox4k-lSEnFw",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 98
+    },
+    __self: this
+  }, "Here"), " is a video of the latest edition, courtesy of", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://www.linkedin.com/in/iboyadzhieva/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 100
+    },
+    __self: this
+  }, "Inna Boyadzhieva"), "."))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_column__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    max: 3,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 108
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_card__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    title: "Sea Turtles",
+    image: __webpack_require__(/*! ../../../static/images/what/speak/lampedusa_sexy_turtles.jpg */ "./static/images/what/speak/lampedusa_sexy_turtles.jpg"),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 109
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 112
+    },
+    __self: this
+  }, "A 15-minute guided tour of the", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ext_link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    to: "https://www.lampedusaturtlerescue.org/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 114
+    },
+    __self: this
+  }, "Lampedusa Turtle Rescue Center"), ", with information about the different species of sea turtles, their lifecycle, their behavior, and the threats that they are facing because of humans."))))));
+});
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/what/speak/conferences")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=conferences.js.6aab6dce29ba520ee8da.hot-update.js.map
