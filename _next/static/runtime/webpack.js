3c808793414d814c0dc755ec5f55137b7e6163d4
IceOnFire <antony.mistretta@gmail.com>
2020-08-05 22:55:59 +0200

    Update slogan

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "523d6feb755df8798202";
+/******/ 	var hotCurrentHash = "a899a5e4cdcef0c318ec";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
