e44480e583dc032728b9fac8320957353c441fad
IceOnFire <antony.mistretta@gmail.com>
2021-06-17 16:49:26 +0200

    Add new project and social icon

--- _next/static/chunks/styles.js
+++ _next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1615203790283");
+            injectCss(link, link.href.split("?")[0] + "?unix=1623936580892");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1615203790277");
+            injectCss(link, link.href.split("?")[0] + "?unix=1623936580885");
           });
       }
     }
---
+++ _next/static/webpack/static/development/pages/what/code.js.0aa9f7515b6bc97f08f4.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/what/code.js",{
+
+/***/ "./static/images/what/code/react-quiz-game.png":
+/*!*****************************************************!*\
+  !*** ./static/images/what/code/react-quiz-game.png ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/react-quiz-game-e6c20e90ffb2053bce9d842c11509103.png";
+
+/***/ })
+
+})
+//# sourceMappingURL=code.js.0aa9f7515b6bc97f08f4.hot-update.js.map
